60acdb861fe2e1761613335c07db7152dc060003
riceball <lten10000@gmai.com>
2026-04-27 17:41:45 +0900

    typescript3

---
+++ dist/exercise-type-guard.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const notify = (appNotification) => {
+    if ('sendEmail' in appNotification) {
+        appNotification.sendEmail();
+    }
+    else if ('sendPush' in appNotification) {
+        appNotification.sendPush();
+    }
+};
+const emailNotification = {
+    type: 'email',
+    emailAddress: 'taro.samurai@example.com',
+    sendEmail: () => {
+        console.log('メール通知を送信しました。');
+    },
+};
+const pushNotification = {
+    type: 'push',
+    deviceToken: 'abc123xyz',
+    sendPush: () => {
+        console.log('プッシュ通知を送信しました。');
+    },
+};
+notify(emailNotification);
+notify(pushNotification);
